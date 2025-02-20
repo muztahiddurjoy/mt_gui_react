3bb6013d26aaeec952ce243d3df1a15a3b63d210
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-02-20 13:58:54 +0600

    feat: migrate project to Next.js, add new configuration files, and implement utility functions

--- client/components /MapContainer.tsx
+++ client/components /MapContainer.tsx
@@ -2,16 +2,17 @@
 import { MapContainer as Container, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import React from 'react'
 import "leaflet/dist/leaflet.css";
-
+import 'leaflet.offline';
+import 'leaflet/dist/leaflet.css';
 const MapContainer = () => {
   return (
     <div>
-        <Container center={[51.505, -0.09]} style={{ height: '100vh' }} zoom={13} scrollWheelZoom={true}>
+        <Container center={[23.772794613186193, 90.4253266921477]} style={{ height: '100vh' }} zoom={100} scrollWheelZoom={true}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={[23.772794613186193, 90.4253266921477]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
